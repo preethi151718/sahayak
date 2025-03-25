4037c49fd94249a20fa2c3deeade6aa765eaa649
Bharathi Shankar J <bharathishankarj@cookr.in>
2025-03-25 23:47:48 +0530

    Added Front Page with Logo

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        splash: 'index.html', // Splash screen
+        main: 'main.html',     // React app
+      },
+    },
   },
 });
